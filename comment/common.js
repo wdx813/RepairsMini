1fae96fb8dd2b6e9344021526330d82f689bee95
wdx813 <2280291661@qq.com>
2018-07-12 17:59:09 +0800

    登录页设计完成

---
+++ comment/common.js
@@ -0,0 +1,56 @@
+const utils = require('../utils/util.js')
+const baseUrl = 'https://www.wgxy.org/core'
+/**
+ * 用户登录
+ */
+function login(url) {
+    return sendRequest(url, 'GET').then(res => res.data)
+}
+
+/**
+ * 用户注册
+ */
+function register(url) {
+    return sendRequest(url, 'GET').then(res => res.data)
+}
+
+/**
+ * 封装请求函数
+ */
+function sendRequest(url, method, data) {
+    var token = wx.getStorageSync('token')
+    return new Promise((resole, reject) => {
+        wx.request({
+            url: baseUrl + url,
+            method: method,
+            data: data,
+            header: {
+                'content-type': 'application/json',
+                'Authorization': token
+            },
+            success: resole,
+            fail: reject
+        })
+    })
+}
+
+/**
+ * 上传文件
+ */
+function uploadFile(url, data, filePath) {
+    return new Promise((resole, reject) => {
+        wx.uploadFile({
+            url: baseUrl + url,
+            filePath: filePath,
+            name: 'file',
+            formData: data,
+            success: resole,
+            fail: reject
+        })
+    })
+}
+
+
+module.exports = {
+    login
+}
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,66 +1,117 @@
 // pages/login/login.js
+const common = require('../../comment/common.js')
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        title: '欢迎登录报修平台'
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+    onLogin: function(e) {
+        var loginName = e.detail.value.loginName.trim()
+        var password = e.detail.value.password.trim()
+        if(!loginName || loginName == "") {
+            wx.showToast({
+                title: '登录名不能为空',
+                icon: 'none',
+                duration: 2000
+            })
+            return
+        }
+        if(!password || password == "") {
+            wx.showToast({
+                title: '密码不能为空',
+                icon: 'none',
+                duration: 2000
+            })
+            return
+        }
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+        common.login('/appuser/appuserlogin?loginName=' + loginName + '&password=' + password).then(res => {
+            console.log(res)
+            if(res.code == 0) {
+                wx.setStorageSync('token', res.token)
+                wx.switchTab({
+                    url: '/pages/index/index',
+                })
+            } else if(res.code == 2) {
+                wx.showToast({
+                    title: res.msg,
+                    icon: 'none',
+                    duration: 2000
+                })
+            } else {
+                wx.showToast({
+                    title: '服务器错误',
+                    icon: 'none',
+                    duration: 2000
+                })
+            }
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
+    toRegister: function() {
+        wx.navigateTo({
+            url: '/pages/register/register',
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
 })
